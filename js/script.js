2d3396d6ccd943bc3218a3adfcfd6ce1279ef00f
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-08 00:31:19 +0300

    24/36

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-function checkIfCanAccessContent(subType) {
-    const canAccessContent = subType==="pro" || subType==="vip"; // Change this line
-  
-    return canAccessContent;
+function getDiscount(totalSpent) {
+    const BASE_DISCOUNT = 0;
+    const BRONZE_DISCOUNT = 0.02;
+    const SILVER_DISCOUNT = 0.05;
+    const GOLD_DISCOUNT = 0.1;
+    let discount;
+    // Change code below this line
+  if (totalSpent >= 50000) {
+    discount = GOLD_DISCOUNT;
+  } else if (totalSpent >= 20000) {
+    discount = SILVER_DISCOUNT;
+  } else if (totalSpent >= 5000) {
+  discount = BRONZE_DISCOUNT;
+  } else {
+    discount = BASE_DISCOUNT
   }
+    // Change code above this line
+    return discount;
+  }
+  
