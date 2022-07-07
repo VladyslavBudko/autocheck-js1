773751bd9608369b9ca3cfc30b997c9c72023bce
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-08 00:22:09 +0300

    22/36

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
-function checkStorage(available, ordered) {
-  let message;
-  // Change code below this line
-  if (ordered === 0) {
-    message = "There are no products in the order!";
-  } else if (ordered > available) {
-    message = "Your order is too large, there are not enough items in stock!";
-  } else {
-    message = "The order is accepted, our manager will contact you";
+function checkIfCanAccessContent(subType) {
+    const canAccessContent = subType==="pro" || subType==="vip"; // Change this line
+  
+    return canAccessContent;
   }
-
-  // Change code above this line
-  return message;
-}
