c3a6b836bc2e3276b288fc049dcfc307c0db35a5
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-08 00:42:08 +0300

    27/36

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 function getDiscount(totalSpent) {
-    const BASE_DISCOUNT = 0;
-    const BRONZE_DISCOUNT = 0.02;
-    const SILVER_DISCOUNT = 0.05;
-    const GOLD_DISCOUNT = 0.1;
-    let discount;
-    // Change code below this line
+  const BASE_DISCOUNT = 0;
+  const BRONZE_DISCOUNT = 0.02;
+  const SILVER_DISCOUNT = 0.05;
+  const GOLD_DISCOUNT = 0.1;
+  let discount;
+  // Change code below this line
   if (totalSpent >= 50000) {
     discount = GOLD_DISCOUNT;
   } else if (totalSpent >= 20000) {
     discount = SILVER_DISCOUNT;
   } else if (totalSpent >= 5000) {
-  discount = BRONZE_DISCOUNT;
+    discount = BRONZE_DISCOUNT;
   } else {
-    discount = BASE_DISCOUNT
+    discount = BASE_DISCOUNT;
   }
-    // Change code above this line
-    return discount;
-  }
-  
+  // Change code above this line
+  return discount;
+}
