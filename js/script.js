67c22976c22caf800c19707367f0c914e3621c59
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-18 22:51:39 +0300

    2.21

--- js/script.js
+++ js/script.js
@@ -1,11 +1,38 @@
-function makeArray(firstArray, secondArray, maxLength) {
+function findLongestWord(string) {
   // Change code below this line
 
-const mix = firstArray.concat(secondArray);
-console.log(mix);
-return mix.slice(0, maxLength)
+  // const wordArray = Array.from(arguments);
+
+  const wordArray = string.split(" ");
+
+  console.log(wordArray);
+
+  let i = 0;
+  let maxLength = 0;
+  let numberOfElement = 0;
+  let maxWord = '';
+
+  console.log("length = ", wordArray.length);
+
+  for (let i = 0; i < wordArray.length; i += 1) {
+    if (maxLength < wordArray[i].length) {
+      maxLength = wordArray[i].length;
+maxWord = wordArray[i];
+
+      console.log("element ", i);
+      console.log("length", wordArray[i].length);
+
+      let numberOfElement = i;
+      console.log(wordArray[numberOfElement]);
+    }
+  }
+
+  console.log("max word is", maxWord);
+  return maxWord;
 
   // Change code above this line
 }
 
-makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4);
+findLongestWord("The quick brown fox jumped over the lazy dog");
+
+
