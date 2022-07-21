07295e0859ed22412e00eeec8327859142abef7c
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-22 00:13:32 +0300

    2.16

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-const apartment = {
-  descr: "Spacious apartment in the city center",
-  rating: 4,
-  price: 2153,
-};
-// Change code below this line
-const keys = Object.keys(apartment) ;
-const values = Object.values(apartment) ;
+function countTotalSalary(salaries) {
+  let totalSalary = 0;
+  // Change code below this line
+const keys = Object.keys(salaries);
+const values = Object.values(salaries);
+for (const value of values) {
+  totalSalary += value;
+}
 
+  // Change code above this line
+  return totalSalary;
+}
