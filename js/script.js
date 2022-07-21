1b10ccfc40ac43a32a79a055125bd46218c2cf9a
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-21 23:58:24 +0300

    2.15

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
-function countProps(object) {
-  // Change code below this line
-  let propCount = 0;
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+// Change code below this line
+const keys = Object.keys(apartment) ;
+const values = Object.values(apartment) ;
 
-const keys = Object.keys(object);
-  for (const key of keys) {
-      propCount += 1;
-    }
-  
-
-  return propCount;
-  // Change code above this line
-}
