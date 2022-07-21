6de154e581ca87680f44084808662d71464ea142
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-22 00:18:03 +0300

    2.17

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-function countTotalSalary(salaries) {
-  let totalSalary = 0;
-  // Change code below this line
-const keys = Object.keys(salaries);
-const values = Object.values(salaries);
-for (const value of values) {
-  totalSalary += value;
-}
+const colors = [
+  { hex: "#f44336", rgb: "244,67,54" },
+  { hex: "#2196f3", rgb: "33,150,243" },
+  { hex: "#4caf50", rgb: "76,175,80" },
+  { hex: "#ffeb3b", rgb: "255,235,59" },
+];
 
-  // Change code above this line
-  return totalSalary;
+const hexColors = [];
+const rgbColors = [];
+// Change code below this line
+for (const color of colors) {
+  hexColors.push(color.hex);
+  rgbColors.push(color.rgb);
 }
