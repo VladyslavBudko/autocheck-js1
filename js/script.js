8c73f859d95da6c9783020364bdaf63e742b2bef
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-21 23:22:30 +0300

    2.9

--- js/script.js
+++ js/script.js
@@ -1,38 +1,10 @@
-function findLongestWord(string) {
+const emailInputName = "email";
+const passwordInputName = "password";
+
+const credentials = {
   // Change code below this line
-
-  // const wordArray = Array.from(arguments);
-
-  const wordArray = string.split(" ");
-
-  console.log(wordArray);
-
-  let i = 0;
-  let maxLength = 0;
-  let numberOfElement = 0;
-  let maxWord = '';
-
-  console.log("length = ", wordArray.length);
-
-  for (let i = 0; i < wordArray.length; i += 1) {
-    if (maxLength < wordArray[i].length) {
-      maxLength = wordArray[i].length;
-maxWord = wordArray[i];
-
-      console.log("element ", i);
-      console.log("length", wordArray[i].length);
-
-      let numberOfElement = i;
-      console.log(wordArray[numberOfElement]);
-    }
-  }
-
-  console.log("max word is", maxWord);
-  return maxWord;
+[emailInputName] : "henry.carter@aptmail.com",
+[passwordInputName] : "jqueryismyjam",
 
   // Change code above this line
-}
-
-findLongestWord("The quick brown fox jumped over the lazy dog");
-
-
+};
