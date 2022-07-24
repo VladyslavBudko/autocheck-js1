75ba0a5bca2c312d4f10bd6c3dad2ca5295301b0
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-24 22:30:08 +0300

    3.33

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
-function makeTask(data) {
-  const completed = false;
-  const category = "General";
-  const priority = "Normal";
-  // Change code below this line
-  const newTask = {completed, category, priority, ...data};
-return newTask;
+// Change code below this line
+const firstElement = [];
+const args = [];
+
+function findMatches(firstElement, ...args) {
+  const matches = []; // Don't change this line
+
+  console.log("first: ", firstElement);
+  console.log("args: ", args);
+
+  for (const arg of args) {
+    if (firstElement.includes(arg)) {
+      matches.push(arg);
+      console.log(`${arg} include`);
+    }
+  }
+
   // Change code above this line
+  console.log(matches);
+  return matches;
 }
+
+findMatches([4, 89, 17, 36, 2], 8, 17, 89, 27, 2);
