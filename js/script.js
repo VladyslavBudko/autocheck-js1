05dcc0c2e583c7a2960b2f3356114f21e077bf47
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-24 22:56:12 +0300

    3.34

--- js/script.js
+++ js/script.js
@@ -1,23 +1,18 @@
-// Change code below this line
-const firstElement = [];
-const args = [];
-
-function findMatches(firstElement, ...args) {
-  const matches = []; // Don't change this line
-
-  console.log("first: ", firstElement);
-  console.log("args: ", args);
-
-  for (const arg of args) {
-    if (firstElement.includes(arg)) {
-      matches.push(arg);
-      console.log(`${arg} include`);
-    }
-  }
+const bookShelf = {
+  // Change code below this line
+  books: ["The last kingdom", "The guardian of dreams"],
+  getBooks() {
+    return "Returning all books";
+  },
+  addBook(bookName) {
+    return `Adding book ${bookName}`;
+  },
+  removeBook(bookName) {
+    return `Deleting book ${bookName}`
+  },
+  updateBook(oldName, newName) {
+    return `Updating book ${oldName} to ${newName}`
+  },
 
   // Change code above this line
-  console.log(matches);
-  return matches;
-}
-
-findMatches([4, 89, 17, 36, 2], 8, 17, 89, 27, 2);
+};
