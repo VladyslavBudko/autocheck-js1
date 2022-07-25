fe2baa3343092ce7ccef17e5bc7128be4d1e5e07
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-25 03:32:47 +0300

    3.41-1,2

--- js/script.js
+++ js/script.js
@@ -1,12 +1,49 @@
 const atTheOldToad = {
-  potions: ["Speed potion", "Dragon breath", "Stone skin"],
-  updatePotionName(oldName, newName) {
-    // Change code below this line
-if (this.potions.includes(oldName)) {
-  const item = this.potions.indexOf(oldName);
-  this.potions.splice(item, 1, newName);
-}
-
-    // Change code above this line
+  potions: [
+    { name: "Speed potion", price: 460 },
+    { name: "Dragon breath", price: 780 },
+    { name: "Stone skin", price: 520 },
+  ],
+  // Change code below this line
+  getPotions() {
+    return this.potions;
   },
+  addPotion(newPotion) {
+    for (const potion of this.potions) {
+      if (potion.name === newPotion.name) {
+        return `Error! Potion ${newPotion.name} is already in your inventory!`;
+      }
+    }
+    this.potions.push(newPotion);
+
+  },
+
+  
+  removePotion(potionName) {
+    let potionIndex = -1;
+
+    for (let i = 0; i < this.potions.length; i += 1) {
+      const potion = this.potions[i] ;
+
+      if (potion.name === potionName) {
+        potionIndex = i;
+        this.potions.splice(potionIndex, 1);
+        return `Potion ${potionName.name} is removed!`
+      }
+    }
+    if (potionIndex === -1) {
+      return `Potion ${potionName.name} is not in inventory!`;
+    }
+  },
+
+  updatePotionName(oldName, newName) {
+    const potionIndex = this.potions.indexOf(oldName);
+
+    if (potionIndex === -1) {
+      return `Potion ${oldName} is not in inventory!`;
+    }
+
+    this.potions.splice(potionIndex, 1, newName);
+  },
+  // Change code above this line
 };
