ab9ae227f594a8212f859b0f90c924bdaccf21f7
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-29 00:36:03 +0300

    4.8

--- js/script.js
+++ js/script.js
@@ -1,19 +1,6 @@
-function getCommonElements(firstArray, secondArray) {
-  const commonElements = [];
-  // Change code below this line
+// Change code below this line
 
-  // for (let i = 0; i < firstArray.length; i += 1) {
-  //   if (secondArray.includes(firstArray[i])) {
-  //     commonElements.push(firstArray[i]);
-  //   }
-  // }
-
-  firstArray.forEach( (elem1) => {
-    if (secondArray.includes(elem1)) {
-      commonElements.push(elem1);
-    }
-  });
-
-  return commonElements;
+const calculateTotalPrice = (quantity, pricePerItem) => {
   // Change code above this line
+  return quantity * pricePerItem;
 }
