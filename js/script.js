2b45e875b8620c6ed645b2de333167aadd9b6562
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-29 00:42:01 +0300

    4.10

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 // Change code below this line
-const calculateTotalPrice = (quantity, pricePerItem) =>  quantity * pricePerItem;
+const calculateTotalPrice = (orderedItems) => {
+  let totalPrice = 0;
 
+  orderedItems.forEach((item) => {
+    totalPrice += item;
+  });
+
+  return totalPrice;
+}
 // Change code above this line
