1b44b480d5204660a72cc9b2fa2447ff98b59a8d
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-29 00:00:19 +0300

    4.5-1

--- js/script.js
+++ js/script.js
@@ -1,30 +1,11 @@
-const pizzaPalace = {
-  pizzas: ["Ultracheese", "Smoked", "Four meats"],
-  order(pizzaName, onSuccess, onError) {
-    if (this.pizzas.includes(pizzaName)) {
-      return onSuccess(pizzaName);
-    };
-    return onError(
-      `There is no pizza with a name ${pizzaName} in the assortment.`
-    );
-  },
-};
-// Change code above this line
+function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
+  // Change code below this line
 
-// Callback for onSuccess
-function makePizza(pizzaName) {
-  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+  for (let i = 0; i < orderedItems.length; i += 1) {
+    totalPrice += orderedItems[i];
+  }
+
+  // Change code above this line
+  return totalPrice;
 }
-
-// Callback for onError
-function onOrderError(error) {
-  return `Error! ${error}`;
-}
-
-// Method calls with callbacks
-pizzaPalace.order("Smoked", makePizza, onOrderError);
-pizzaPalace.order("Four meats", makePizza, onOrderError);
-pizzaPalace.order("Big Mike", makePizza, onOrderError);
-pizzaPalace.order("Vienna", makePizza, onOrderError);
-
-
