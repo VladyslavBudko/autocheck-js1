819b2ff2375ee291c5047c58be559e5bb4e1baec
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-30 11:40:32 +0300

    4.16

--- js/script.js
+++ js/script.js
@@ -2,22 +2,21 @@ const books = [
   {
     title: "The Last Kingdom",
     author: "Bernard Cornwell",
-    rating: 8.38,
+    genres: ["adventure", "history"],
   },
   {
     title: "Beside Still Waters",
     author: "Robert Sheckley",
-    rating: 8.51,
+    genres: ["fiction"],
   },
   {
-    title: "The Dream of a Ridiculous Man",
-    author: "Fyodor Dostoevsky",
-    rating: 7.75,
+    title: "Redder Than Blood",
+    author: "Tanith Lee",
+    genres: ["horror", "mysticism"],
   },
-  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
-  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
 ];
 // Change code below this line
 
-const titles = books.map( book => book.title);
-console.log(titles);
+const genres = books.flatMap(book => book.genres);
+
+console.log(genres);
