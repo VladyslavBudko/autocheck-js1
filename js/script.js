d2c9600e2bd0eef392dda9c6186bc86645a9757a
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-30 11:29:50 +0300

    4.14

--- js/script.js
+++ js/script.js
@@ -1,23 +1,4 @@
-function changeEven(numbers, value) {
-  // Change code below this line
-  // for (let i = 0; i < numbers.length; i += 1) {
-  //   if (numbers[i] % 2 === 0) {
-  //     numbers[i] = numbers[i] + value;
-  //   }
-  // }
-
-  const newArray = [];
-
-  numbers.forEach((element) => {
-    // if (element % 2 === 0) {
-    //   newArray.push(element + value);
-    // } else {
-    //   newArray.push(element);
-    // }
-
-    element % 2 === 0 ? newArray.push(element + value) : newArray.push(element);
-  });
-
-  return newArray;
-  // Change code above this line
-}
+const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+// Change code below this line
+const planetsLengths = planets.map(planet => planet.length);
+console.log(planetsLengths);
