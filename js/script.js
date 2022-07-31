1cbbc85b44593833bcf9c7a76fbc044c02e1939b
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-31 20:05:27 +0300

    4.22-2

--- js/script.js
+++ js/script.js
@@ -69,7 +69,7 @@ let color = 'Blue';
 // Change code below this line
 const getUsersWithEyeColor = (users, color) => {
   users.filter(user => user.eyeColor === color);
-  console.log(user.eyeColor)
+  // console.log(user.eyeColor)
   return user;
  };
  // Change code above this line
