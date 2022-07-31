de297cfd2e83692dd42d1d98f2d1e199c17d78f8
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-31 23:53:50 +0300

    4.24-1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
-// Change code below this line
-const getUsersWithAge = (users, minAge, maxAge) => {
-  return users.filter(user => user.age > minAge && user.age < maxAge)
+// // Change code below this line
+// const getUsersWithFriend = (users, friendName) => {
+//   return users.filter(
+//      ({friends}) => friends = friendName);
+    
+// };
+// // Change code above this line
 
+
+// const getUsersWithFriend = (users, friendName) => {
+//   users.filter(user => {
+//       let userFriends = [...user.friends];
+//       userFriends.filter(friend) => {
+//         if friend === friendName
+//               return user;
+//       }
+//   })
+// };
+
+
+
+const getUsersWithFriend = (users, friendName) => {
+  users.filter(user => {
+      [...user.friends].filter(friend => {
+        if friend === friendName
+              return user;
+      })
+  })
 };
-// Change code above this line
