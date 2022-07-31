db1b1c97d159b69ac40dd33fa6e8136e2d097fcd
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-31 21:06:47 +0300

    4.23

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-
-
 // Change code below this line
-const getUsersWithEyeColor = (users, color) => {
-  users.filter(user => user.eyeColor === color);
-  console.log(user.eyeColor)
-  return user;
- };
- // Change code above this line
+const getUsersWithAge = (users, minAge, maxAge) => {
+  return users.filter(user => user.age > minAge && user.age < maxAge)
+
+};
+// Change code above this line
