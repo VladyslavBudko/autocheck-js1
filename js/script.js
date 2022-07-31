1e4198f7890712495b6e95f8daa43dfb333791e1
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-07-31 21:01:09 +0300

    4.22 fin

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-[
+let users = [
   {
     name: "Moore Hensley",
     email: "moorehensley@indexia.com",
@@ -69,7 +69,7 @@ let color = 'Blue';
 // Change code below this line
 const getUsersWithEyeColor = (users, color) => {
   users.filter(user => user.eyeColor === color);
-  // console.log(user.eyeColor)
+  console.log(user.eyeColor)
   return user;
  };
  // Change code above this line
