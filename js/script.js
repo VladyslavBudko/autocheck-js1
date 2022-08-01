c1464479b9696933a05d205e4be9e29fd26c9c22
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-01 21:07:24 +0300

    4.34

--- js/script.js
+++ js/script.js
@@ -110,3 +110,19 @@ const isAnyUserActive = users => {
   return users.some(user => user.isActive)
 };
 // Change code above this line
+
+
+// 34
+const players = {
+  mango: 1270,
+  poly: 468,
+  ajax: 710,
+  kiwi: 244
+};
+const playtimes = Object.values(players); // [1270, 468, 710, 244]
+// Change code below this line
+
+const totalPlayTime = playtimes.reduce((acc, value) => acc + value, 0);
+
+// Change code above this line
+const averagePlayTime = totalPlayTime / playtimes.length;
