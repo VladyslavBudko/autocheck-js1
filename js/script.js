8337ad1908ab3af912e8d16c9d6917dcae8fbdb7
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-01 20:53:24 +0300

    4.30

--- js/script.js
+++ js/script.js
@@ -51,3 +51,34 @@ const AUTHOR = 'Robert Sheckley';
 const bookWithTitle = books.find(book => book.title === BOOK_TITLE);
 const bookByAuthor = books.find(book => book.author === AUTHOR);
 
+
+
+//29 Change code below this line
+const getUserWithEmail = (users, email) => {
+  return users.find(user => user.email === email);
+};
+
+
+// v2
+const getUserWithEmail = (users, email) => users.find(user => user.email === email);
+
+
+
+//30 Change code above this line
+
+
+const firstArray = [26, 94, 36, 18];
+const secondArray = [17, 61, 23];
+const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// Change code below this line
+
+const eachElementInFirstIsEven = firstArray.every(isEven => isEven % 2 === 0);
+const eachElementInFirstIsOdd = firstArray.every(isOdd => isOdd % 2 !==0
+);
+
+const eachElementInSecondIsEven = secondArray.every(isEven => isEven % 2 === 0);
+const eachElementInSecondIsOdd = secondArray.every(isOdd => isOdd % 2 !==0);
+
+const eachElementInThirdIsEven = thirdArray.every(isEven => isEven % 2 === 0);
+const eachElementInThirdIsOdd = thirdArray.every(isOdd => isOdd % 2 !==0);
+// 
