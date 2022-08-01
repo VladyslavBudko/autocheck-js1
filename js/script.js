47fbcebdc1946d7418be56d18a1ad49ee8f3b11a
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-01 19:56:42 +0300

    4.26

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-// Change code below this line
+//25  Change code below this line
 const getFriends = (users) => users
     .flatMap((user) => user.friends)
     .filter((user, index, array) => array.indexOf(user) === index);
 // Change code above this line
+
+
+//26  Change code below this line
+const getActiveUsers = (users) => users.filter(user => user.isActive);
+   
+// Change code above this line
+
+
