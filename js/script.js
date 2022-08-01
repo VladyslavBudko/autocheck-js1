bfffbc5af5eccb4f071c67b279eaac42b867a33a
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-01 21:13:24 +0300

    4.35

--- js/script.js
+++ js/script.js
@@ -126,3 +126,16 @@ const totalPlayTime = playtimes.reduce((acc, value) => acc + value, 0);
 
 // Change code above this line
 const averagePlayTime = totalPlayTime / playtimes.length;
+
+
+// 35
+const players = [
+  { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+  { name: "Poly", playtime: 469, gamesPlayed: 2 },
+  { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+  { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+];
+// Change code below this line
+
+const totalAveragePlaytimePerGame = players.reduce((averege, player) =>
+player.playtime / player.gamesPlayed + averege, 0);
